11cfd04a8bf539649fc4adbce00e302249efe195
Станислав Коротаев <superstals@gmail.com>
2013-02-11 00:49:36 -0800

    Create gameoverlabel.h

---
+++ Words-cocos2dx/Words/Words/proj.win32/gameoverlabel.h
@@ -0,0 +1,29 @@
+#pragma once 
+
+#include "cocos2d.h"
+using namespace cocos2d;
+
+class GameOverLayer : public cocos2d::CCLayer{
+public:
+  GameOverLayer(int score);
+	~GameOverLayer();
+
+private:
+	// Полупрозрачный слой размером с экран
+	cocos2d::CCLayerColor *bg;
+	cocos2d::CCLaberTTF *scoreLabel;
+	cocos2d::CCMenu *menu;
+
+	void setupBackground();
+	void setupScore(int score);
+	void setupButtons();
+
+	void newGameAction(CCObject *pSender);
+	void mainMenuAction(CCObject *pSender);
+
+    virtual bool ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent);
+	virtual void ccTouchMoved(CCTouch *pTouch, CCEvent *pEvent);
+	virtual void ccTouchEnded(CCTouch *pTouch, CCEvent *pEvent);
+	virtual void ccTouchCancelled(CCTouch *pTouch, CCEvent *pEvent);
+    virtual void registerWithTouchDispatcher();
+};
